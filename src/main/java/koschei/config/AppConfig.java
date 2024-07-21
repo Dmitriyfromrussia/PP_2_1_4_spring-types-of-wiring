5f4a04f482cfec9ccd2589a5d6a320a0da96a958
2.1.3_Spring_HelloWorld <Dmitriyfromrussia@gmail.com>
2024-07-21 16:35:39 +0200

    направляю решение на проверку. Интересно, правильное ли суждение в комментарии к строке 10 Duck5.class

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "koschei")
+
 public class AppConfig {
 
     @Bean
--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,12 +1,16 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
 
+    @Autowired
+    private Egg6 egg; //понимаю, что лучше связывать объекты через конструктор, тк в данном случае не возможно закрыть сделать поле final
+
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + egg.toString();
     }
 }
